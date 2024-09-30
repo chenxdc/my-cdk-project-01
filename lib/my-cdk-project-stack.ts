06c26c76bb6d7a7b21f6e34ea0aa06905918390d
Xudong <chenxdc@foxmail.com>
2024-09-30 14:13:47 -0400

    Add API Gateway with Lambda.

--- lib/my-cdk-project-stack.ts
+++ lib/my-cdk-project-stack.ts
@@ -2,23 +2,41 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 
 export class MyCdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    //defines an AWS Lambda resource
+    const hello = new Function(this, "HelloHandler", {
+      runtime: Runtime.NODEJS_16_X,
+      code: Code.fromAsset("lambda"),
+      handler: "hello.handler",
+    });
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    const gateway = new LambdaRestApi(this, "Endpoint", {
+      handler: hello,
+    });
+
+
+
+
+
 
     // example resource
     // const queue = new sqs.Queue(this, 'MyCdkProjectQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
-    const myLambda = new lambda.Function(this, 'lambdaFunction', {
+    /*const myLambda = new lambda.Function(this, 'lambdaFunction', {
       functionName: 'first-cdk-lambda',
       code: new lambda.AssetCode('src'),
       handler: 'index.handler',
       runtime: lambda.Runtime.NODEJS_16_X,
       memorySize: 128
-    })
+    })*/
   }
 }
